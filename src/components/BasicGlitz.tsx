29f5d91a7884b256a8e56dc563a76571e4e421a5
truptigaonkar <trups11.trupti@gmail.com>
2020-12-01 16:32:54 +0100

    Glitz Basic styling added with margin, padding, border

--- src/components/BasicGlitz.tsx
+++ src/components/BasicGlitz.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { styled } from '@glitz/react';
 
-const BasicGlitz = () => {
+const BasicGlitz = (props:any) => {
+    const [backgroundColor, setBackgroundColor] = useState('papayawhip');
+    function onClickButton() {
+        setBackgroundColor(backgroundColor === 'papayawhip' ?'lightblue' : 'papayawhip');
+    }
     return (
         <div>
             <h5>Basic</h5>
             <p>This example creates two simple components, a wrapper and a title, with some styles attached to it:</p>
-                <Wrapper>
-                    <Title>Hover me!</Title>
-                </Wrapper>
+            <Wrapper style={{ backgroundColor }}>
+                <Title>Hover <styled.Span css={{ color: 'red' }}>me!</styled.Span></Title>
+                <Button onClick={onClickButton}>Change background color</Button>
+            </Wrapper>
         </div>
     )
 }
@@ -17,15 +22,45 @@ export default BasicGlitz
 
 const Wrapper = styled.section({
     padding: {xy: '4em'},
-    backgroundColor: 'papayawhip'
+    backgroundColor: 'papayawhip',
+    display:'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
 })
 
 const Title = styled.h1({
     fontSize: '1.5em',
-    textAlign: 'center',
     color: 'palevioletred',
     ':hover': {
         cursor: 'pointer',
         color:'green',
     },
+})
+
+const Button = styled.button({
+    fontSize: '1em',
+    margin: { xy: '2em' },
+    //    margin: {
+    //     top: '20px',
+    //     right: '20px',
+    //     bottom: '20px',
+    //     left: '10px',
+    //   },
+    border:
+    {
+        xy:
+        {
+            width: 2,
+            style: 'solid',
+            color: 'palevioletred'
+        }
+    }, //border: 2px solid palevioletred;
+    padding: {
+        x: '1em',
+        y: '.25em',
+    }, //padding: 0.25em 1em;
+    borderRadius: '3px',
+    // width: ['50%', 'fit-content'],
+    width: [100, 'max-content'],
 })
