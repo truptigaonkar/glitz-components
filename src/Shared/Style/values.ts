76fd4de74248de54698dba8b7d02b9618ab60137
truptigaonkar <trups11.trupti@gmail.com>
2020-12-02 14:36:42 +0100

    Styling components added for margin, padding, border, colors

---
+++ src/Shared/Style/values.ts
@@ -0,0 +1,13 @@
+// Border
+export const thin = 1;
+export const thick = 2;
+
+// Margin & padding
+export const tiny = 4;
+export const small = 8;
+export const medium = 16;
+export const large = 24;
+export const huge = 32;
+export const gigantic = 40;
+export const humongous = 48;
+export const moderate = large; // This is default spacing, e.g. margin between `<p>`
--- src/components/BasicGlitz.tsx
+++ src/components/BasicGlitz.tsx
@@ -1,17 +1,20 @@
 import React, {useState} from 'react'
 import { styled } from '@glitz/react';
+import { huge, humongous, large, medium, small } from '../Shared/Style';
+import * as style from '../Shared/Style';
+import { black, monochromeLight, neutralDark, positiveLight, secondaryDark, tertiaryDark, white } from '../Shared/Style';
 
 const BasicGlitz = (props:any) => {
-    const [backgroundColor, setBackgroundColor] = useState('papayawhip');
+    const [backgroundColor, setBackgroundColor] = useState(neutralDark);
     function onClickButton() {
-        setBackgroundColor(backgroundColor === 'papayawhip' ?'lightblue' : 'papayawhip');
+        setBackgroundColor(backgroundColor === neutralDark ? positiveLight : neutralDark);
     }
     return (
         <div>
             <h5>Basic</h5>
-            <p>This example creates two simple components, a wrapper and a title, with some styles attached to it:</p>
+            <p>This example creates two simple components, a wrapper a title, a button with some styles attached to it: margin, padding, border, </p>
             <Wrapper style={{ backgroundColor }}>
-                <Title>Hover <styled.Span css={{ color: 'red' }}>me!</styled.Span></Title>
+                <Title>Hover <styled.Span css={{ color: tertiaryDark }}>me!</styled.Span></Title>
                 <Button onClick={onClickButton}>Change background color</Button>
             </Wrapper>
         </div>
@@ -21,8 +24,8 @@ const BasicGlitz = (props:any) => {
 export default BasicGlitz
 
 const Wrapper = styled.section({
-    padding: {xy: '4em'},
-    backgroundColor: 'papayawhip',
+    padding: {xy: humongous},
+    backgroundColor: neutralDark,
     display:'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -31,36 +34,54 @@ const Wrapper = styled.section({
 
 const Title = styled.h1({
     fontSize: '1.5em',
-    color: 'palevioletred',
+    color: monochromeLight,
     ':hover': {
         cursor: 'pointer',
-        color:'green',
+        color:secondaryDark,
     },
 })
 
 const Button = styled.button({
-    fontSize: '1em',
+    fontSize: '1.2em',
+    /* // Margin different ways
     margin: { xy: '2em' },
-    //    margin: {
-    //     top: '20px',
-    //     right: '20px',
-    //     bottom: '20px',
-    //     left: '10px',
-    //   },
+    margin: {
+        top: '20px',
+        right: '30px',
+        bottom: '20px',
+        left: '30px',
+    },*/
+    marginTop: small,
+    marginRight: medium,
+    marginBottom: small,
+    marginLeft: medium, 
     border:
     {
         xy:
         {
             width: 2,
             style: 'solid',
-            color: 'palevioletred'
+            color: black
         }
     }, //border: 2px solid palevioletred;
+    /* // Padding different ways
     padding: {
         x: '1em',
         y: '.25em',
     }, //padding: 0.25em 1em;
-    borderRadius: '3px',
-    // width: ['50%', 'fit-content'],
-    width: [100, 'max-content'],
+    padding: {
+        top: '20px',
+        right: '30px',
+        bottom: '20px',
+        left: '30px',
+    }, */
+    paddingTop: large,
+    paddingRight: huge,
+    paddingBottom: large,
+    paddingLeft: huge,
+    borderRadius: style.tiny,
+    ':hover':{
+        backgroundColor: black,
+        color: white,
+    }
 })
