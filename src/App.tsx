321012a703aa86c32b5630bfbccb13c5e6282a80
truptigaonkar <trups11.trupti@gmail.com>
2021-01-26 18:00:33 +0100

    Tabs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import './App.css';
 import BasicGlitz from './components/BasicGlitz';
 import Pseudoelement from './components/Pseudoelement';
 import Boxmodel from './components/Boxmodel';
@@ -9,13 +8,18 @@ import HeadersFlexbox from './components/HeadersFlexbox';
 import DynamicStylingUsingProps from './components/DynamicStylingUsingProps';
 import Animation from './components/Animation';
 import Home from './components/Home';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
 
 function App() {
+  const [activeTab, setActiveTab] = useState('1');
+  const toggle = (tab: any) => {
+    if (activeTab !== tab) setActiveTab(tab);
+  }
   return (
     <Router>
-      <ul>
+      {/* <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/header">HeadersFlexbox</Link></li>
         <li><Link to="/animation">Animation</Link></li>
         <li><Link to="/header">HeadersFlexbox</Link></li>
         <li><Link to="/basicGlitz">BasicGlitz</Link></li>
@@ -23,7 +27,7 @@ function App() {
         <li><Link to="/deepStyleComposition">DeepStyleComposition</Link></li>
         <li><Link to="/pseudoelement">Pseudoelement</Link></li>
         <li><Link to="/dynamicStylingUsingProps">DynamicStylingUsingProps</Link></li>
-      </ul> 
+      </ul>
       <Route exact path="/" component={Home}/>
       <Route path="/animation" component={Animation} />
       <Route path="/header" component={HeadersFlexbox} />
@@ -32,7 +36,142 @@ function App() {
       <Route path="/deepStyleComposition" component={DeepStyleComposition} />
       <Route path="/pseudoelement" component={Pseudoelement} />
       <Route path="/dynamicStylingUsingProps" component={DynamicStylingUsingProps} success color="blue"/>
-      <DynamicStylingUsingProps success color="blue"/>
+      <DynamicStylingUsingProps success color="blue"/> */}
+
+      <div>
+        <Nav tabs pills>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === '1' })}
+              onClick={() => { toggle('1'); }}
+            >
+              <li><Link to="/">Home</Link></li>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === '2' })}
+              onClick={() => { toggle('2'); }}
+            >
+              <li><Link to="/animation">Animation</Link></li>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === '3' })}
+              onClick={() => { toggle('3'); }}
+            >
+              <li><Link to="/header">HeadersFlexbox</Link></li>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === '4' })}
+              onClick={() => { toggle('4'); }}
+            >
+              <li><Link to="/basicGlitz">BasicGlitz</Link></li>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === '5' })}
+              onClick={() => { toggle('5'); }}
+            >
+              <li><Link to="/boxmodel">Boxmodel</Link></li>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === '6' })}
+              onClick={() => { toggle('6'); }}
+            >
+              <li><Link to="/deepStyleComposition">DeepStyleComposition</Link></li>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === '7' })}
+              onClick={() => { toggle('7'); }}
+            >
+              <li><Link to="/pseudoelement">Pseudoelement</Link></li>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === '8' })}
+              onClick={() => { toggle('8'); }}
+            >
+              <li><Link to="/dynamicStylingUsingProps">DynamicStylingUsingProps</Link></li>
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 1 Contents</h4>
+                <Route exact path="/" component={Home} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 2  Contents</h4>
+                <Route path="/animation" component={Animation} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="3">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 3  Contents</h4>
+                <Route path="/header" component={HeadersFlexbox} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="4">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 4  Contents</h4>
+                <Route path="/basicGlitz" component={BasicGlitz} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="5">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 5  Contents</h4>
+                <Route path="/boxmodel" component={Boxmodel} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="6">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 6  Contents</h4>
+                <Route path="/deepStyleComposition" component={DeepStyleComposition} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="7">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 7  Contents</h4>
+                <Route path="/pseudoelement" component={Pseudoelement} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="8">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 8  Contents</h4>
+                <Route path="/dynamicStylingUsingProps" component={DynamicStylingUsingProps} success color="blue" />
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
+      </div>
     </Router>
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
 
 const Home = () => {
+    
     return (
         <>
-                   
+    
                 
             
         </>
